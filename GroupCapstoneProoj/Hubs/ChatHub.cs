a6293f09e19663ba4dad70c8f28a2c7d4fc8c67f
kadirermaya <kadir.ermaya@gmail.com>
2020-08-05 17:00:15 -0700

    updated

--- GroupCapstoneProoj/Hubs/ChatHub.cs
+++ GroupCapstoneProoj/Hubs/ChatHub.cs
@@ -1,20 +1,20 @@
-﻿using Microsoft.AspNet.SignalR;
-using Microsoft.AspNetCore.SignalR;
-using Owin;
-using System.Threading.Tasks;
+﻿//using Microsoft.AspNet.SignalR;
+//using Microsoft.AspNetCore.SignalR;
+//using Owin;
+//using System.Threading.Tasks;
 
-namespace SignalRChat.Hubs
-{
-    public class ChatHub : Microsoft.AspNetCore.SignalR.Hub
-    {
-        public async Task SendMessage(string user, string message)
-        {
-            await Clients.All.SendAsync("ReceiveMessage", user, message);
-        }
+//namespace SignalRChat.Hubs
+//{
+//    public class ChatHub : Microsoft.AspNetCore.SignalR.Hub
+//    {
+//        public async Task SendMessage(string user, string message)
+//        {
+//            await Clients.All.SendAsync("ReceiveMessage", user, message);
+//        }
 
-        public void Send(string userId, string message)
-        {
-            Clients.User(userId).SendAsync(message);
-        }
-    }
-}
+//        public void Send(string userId, string message)
+//        {
+//            Clients.User(userId).SendAsync(message);
+//        }
+//    }
+//}
